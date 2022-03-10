65f0c14b838c3192686528f19a9e8e964e2245c5
William Garcia <williamjbg06@hotmail.com>
2022-03-09 22:08:01 -0600

    formControl

--- frontend/src/app/registro/registro.component.ts
+++ frontend/src/app/registro/registro.component.ts
@@ -9,13 +9,21 @@ import { FormControl } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   nombre = new FormControl('David');
-
-
+  apellido = new FormControl('Chavez');
+  correo = new FormControl('david.chavez@unah.hn');
+  password = new FormControl('1234');
+  municipio = new FormControl('Santa Lucia');
+  departamento = new FormControl('Francisco Morazan');
+  telefono = new FormControl('2258-1585');
+  direccion = new FormControl('Colonia nueva Santa Lucia');
+  
+  
 
   constructor() { }
 
   guardar(){
-
+    console.log(this.nombre.value);
+    console.log(this.apellido.value);
   }
 
   ngOnInit(): void {
