fd8ed039235185627912ad1fbc41ec8980547602
William Garcia <williamjbg06@hotmail.com>
2022-03-15 01:49:35 -0600

    se agrego formControl

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  email = new FormControl()
+  passw = new FormControl();
 
   constructor() { }
 
