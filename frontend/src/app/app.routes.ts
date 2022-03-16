66914129a141db4f9a2e3934bd86a9bb69a8f935
JOVM99 <68938367+JOVM99@users.noreply.github.com>
2022-03-15 18:54:33 -0600

    Formulario Contraseña

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { InicioComponent } from "./components/inicio/inicio.component";
 import { LoginComponent } from "./components/login/login.component";
 import { RegistroComponent } from "./components/registro/registro.component";
 import { RecuperarComponent } from "./components/recuperar/recuperar.component";
+import  { FormContraseComponent } from "./components/form-contrase/form-contrase.component";
+
 
 const app_routes: Routes =[
     {path: 'inicio', component: InicioComponent},
+    {path: 'form-contrase', component: FormContraseComponent},
     {path:'login/registro', component: RegistroComponent},
     {path: 'login', component: LoginComponent},
     {path: 'login/recuperar', component: RecuperarComponent},
