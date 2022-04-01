27c3fa394a471353f89be132879d9df682739572
JOVM99 <68938367+JOVM99@users.noreply.github.com>
2022-04-01 17:16:43 -0600

    Frontend Productos

---
+++ frontend/src/app/components/mis-productos/mis-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-productos',
+  templateUrl: './mis-productos.component.html',
+  styleUrls: ['./mis-productos.component.css']
+})
+export class MisProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
