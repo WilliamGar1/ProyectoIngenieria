5daeb5d2d652a0c5d74d7aa58cdae3d37ea72ca3
William Garcia <williamjbg06@hotmail.com>
2022-04-29 20:19:41 -0600

    creacion pagina de historial de denuncias

---
+++ frontend/src/app/components/complaints-history/complaints-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complaints-history',
+  templateUrl: './complaints-history.component.html',
+  styleUrls: ['./complaints-history.component.css']
+})
+export class ComplaintsHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
